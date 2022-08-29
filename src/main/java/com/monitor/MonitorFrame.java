7d8a6acae17ce39b84ea940bf1280c1c61ce2ffd
liudoudou86 <543972930@qq.com>
2022-08-29 15:13:18 +0800

    feat：更新目录结构

--- src/main/java/com/monitor/MonitorFrame.java
+++ src/main/java/com/monitor/MonitorFrame.java
@@ -1,4 +1,4 @@
-package com;
+package com.monitor;
 
 import javax.swing.*;
 import java.awt.*;
--- src/main/java/com/monitor/MonitorSystem.java
+++ src/main/java/com/monitor/MonitorSystem.java
@@ -1,4 +1,4 @@
-package com;
+package com.monitor;
 
 import com.sun.management.OperatingSystemMXBean;
 
